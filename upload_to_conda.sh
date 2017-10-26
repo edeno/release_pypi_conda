93a9c3e7a0859d0ea1026f62a85f5fd9918fd9aa
Eric Denovellis <edeno@bu.edu>
2017-10-26 08:38:25 -0400

    Revert "Maybe don't force the upload..."
    This reverts commit d16783a41a31126bc3da6bb66d4d0f3200c25e1f.

--- upload_to_conda.sh
+++ upload_to_conda.sh
@@ -6,7 +6,7 @@ VERSION=$2
 conda skeleton pypi $PACKAGE
 conda build $PACKAGE --no-anaconda-upload
 conda convert $CONDA_DIR/conda-bld/osx-64/$PACKAGE-$VERSION*.tar.bz2 -o $CONDA_DIR/conda-bld/ --platform all
-anaconda upload $CONDA_DIR/conda-bld/*/$PACKAGE-$VERSION*.tar.bz2
+anaconda upload $CONDA_DIR/conda-bld/*/$PACKAGE-$VERSION*.tar.bz2 --force
 
 rm -r $PACKAGE
 conda build purge
