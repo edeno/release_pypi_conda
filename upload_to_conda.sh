dbfb55d848c284a5b6d408d46ffc58b1d8eded60
Eric Denovellis <edeno@bu.edu>
2019-10-03 14:53:28 -0700

    Allow user to specify operating system.

--- upload_to_conda.sh
+++ upload_to_conda.sh
@@ -3,10 +3,11 @@ CONDA_DIR=~/miniconda3
 PACKAGE=$1
 VERSION=$2
 USER=${3:-edeno}
+OS=${4:-osx-64}
 
 conda skeleton pypi $PACKAGE --version $VERSION
 conda-build $PACKAGE --no-anaconda-upload
-conda convert $CONDA_DIR/conda-bld/osx-64/$PACKAGE-$VERSION*.tar.bz2 -o $CONDA_DIR/conda-bld/ --platform all
+conda convert $CONDA_DIR/conda-bld/$OS/$PACKAGE-$VERSION*.tar.bz2 -o $CONDA_DIR/conda-bld/ --platform all
 anaconda upload $CONDA_DIR/conda-bld/*/$PACKAGE-$VERSION*.tar.bz2 -u $USER
 
 rm -r $PACKAGE
