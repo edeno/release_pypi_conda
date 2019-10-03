c01e4ba17b4735dc3042965d045c2fe9fee3faf8
Eric Denovellis <edeno@bu.edu>
2019-10-03 14:34:59 -0700

    Add user option

--- upload_to_conda.sh
+++ upload_to_conda.sh
@@ -2,11 +2,12 @@
 CONDA_DIR=~/miniconda3
 PACKAGE=$1
 VERSION=$2
+USER=${3:-edeno}
 
 conda skeleton pypi $PACKAGE --version $VERSION
 conda-build $PACKAGE --no-anaconda-upload
 conda convert $CONDA_DIR/conda-bld/osx-64/$PACKAGE-$VERSION*.tar.bz2 -o $CONDA_DIR/conda-bld/ --platform all
-anaconda upload $CONDA_DIR/conda-bld/*/$PACKAGE-$VERSION*.tar.bz2
+anaconda upload $CONDA_DIR/conda-bld/*/$PACKAGE-$VERSION*.tar.bz2 -u $USER
 
 rm -r $PACKAGE
 conda build purge
