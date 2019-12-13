503eb178102338f89a8710ca22c5c8c4b2712947
Eric Denovellis <edeno@bu.edu>
2019-12-13 10:50:29 -0800

    Build packages for python versions 3.6-3.8

--- upload_to_conda.sh
+++ upload_to_conda.sh
@@ -6,7 +6,9 @@ USER=${3:-edeno}
 OS=${4:-osx-64}
 
 conda skeleton pypi $PACKAGE --version $VERSION
-conda-build $PACKAGE --no-anaconda-upload
+conda-build $PACKAGE --no-anaconda-upload --python 3.6
+conda-build $PACKAGE --no-anaconda-upload --python 3.7
+conda-build $PACKAGE --no-anaconda-upload --python 3.8
 conda convert $CONDA_DIR/conda-bld/$OS/$PACKAGE-$VERSION*.tar.bz2 -o $CONDA_DIR/conda-bld/ --platform all
 anaconda upload $CONDA_DIR/conda-bld/*/$PACKAGE-$VERSION*.tar.bz2 -u $USER
 
