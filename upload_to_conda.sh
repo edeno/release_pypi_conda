ff7dc1844d668dbedb200ccfa4f442519cb16eb8
edeno <edeno@bu.edu>
2020-01-06 11:41:28 -0800

    Skip uploading existing packages

--- upload_to_conda.sh
+++ upload_to_conda.sh
@@ -17,7 +17,7 @@ echo "PYTHON 3.8"
 conda-build $PACKAGE --no-anaconda-upload --python 3.8
 
 conda convert $CONDA_DIR/conda-bld/$OS/$PACKAGE-$VERSION*.tar.bz2 -o $CONDA_DIR/conda-bld/ --platform all
-anaconda upload $CONDA_DIR/conda-bld/*/$PACKAGE-$VERSION*.tar.bz2 -u $USER
+anaconda upload $CONDA_DIR/conda-bld/*/$PACKAGE-$VERSION*.tar.bz2 -u $USER --skip-existing
 
 rm -r $PACKAGE
 conda build purge-all
