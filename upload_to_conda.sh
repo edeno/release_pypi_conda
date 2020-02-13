258b2d28afae758d4735e6a7449795966706e9e3
Eric Denovellis <edeno@bu.edu>
2020-02-12 21:25:45 -0800

    Use noarch

--- upload_to_conda.sh
+++ upload_to_conda.sh
@@ -5,19 +5,21 @@ VERSION=$2
 USER=${3:-edeno}
 OS=${4:-osx-64}
 
-conda skeleton pypi $PACKAGE --version $VERSION
+conda skeleton pypi $PACKAGE --version $VERSION --noarch-python
 
-echo "PYTHON 3.6"
-conda-build $PACKAGE --no-anaconda-upload --python 3.6
+# echo "PYTHON 3.6"
+# conda-build $PACKAGE --no-anaconda-upload --python 3.6
 
-echo "PYTHON 3.7"
-conda-build $PACKAGE --no-anaconda-upload --python 3.7
+# echo "PYTHON 3.7"
+# conda-build $PACKAGE --no-anaconda-upload --python 3.7
 
-echo "PYTHON 3.8"
-conda-build $PACKAGE --no-anaconda-upload --python 3.8
+# echo "PYTHON 3.8"
+# conda-build $PACKAGE --no-anaconda-upload --python 3.8
 
-conda convert $CONDA_DIR/conda-bld/$OS/$PACKAGE-$VERSION*.tar.bz2 -o $CONDA_DIR/conda-bld/ --platform all
-anaconda upload $CONDA_DIR/conda-bld/*/$PACKAGE-$VERSION*.tar.bz2 -u $USER
+conda-build $PACKAGE --no-anaconda-upload
+
+# conda convert $CONDA_DIR/conda-bld/$OS/$PACKAGE-$VERSION*.tar.bz2 -o $CONDA_DIR/conda-bld/ --platform all
+anaconda upload $CONDA_DIR/conda-bld/*/$PACKAGE-$VERSION*.tar.bz2 -u $USER --skip
 
 rm -r $PACKAGE
 conda build purge-all
